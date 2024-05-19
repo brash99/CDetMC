030009deba381cc7e31414cb665116af9653ca43
Edward Brash <brash@jlab.org>
2024-05-19 07:08:39 -0400

    Updates

---
+++ runscripts/single_points_jlab.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./AutomateJLab.sh 13 8001 8006 1000 65.0 120.0
+./AutomateJLab.sh 13 8007 8012 1000 66.0 -120.0
+./AutomateJLab.sh 13 8013 8018 1000 0.0 60.0
+./AutomateJLab.sh 13 8019 8024 1000 0.0 -60.0
+./AutomateJLab.sh 13 8025 8030 1000 30.0 90.0
+./AutomateJLab.sh 13 8031 8036 1000 30.0 -90.0
+./AutomateJLab.sh 13 8037 8042 1000 -40.0 0.0
